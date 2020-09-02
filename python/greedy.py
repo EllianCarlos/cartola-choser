a689d84b6d2324e5becc8d3199d0773552247365
elliancarlos <elliancarlos@gmail.com>
2020-09-01 23:57:02 -0300

    Implementação do greedy iniciada

--- python/greedy.py
+++ python/greedy.py
@@ -1,1 +1,12 @@
-print("Comecei")
+# Uma função greedy baseado na média de pontos e em um budget fixo
+# Essa função retorna o custo de uma função
+import math
+
+
+def greedy(df, row = 0, paramToMaximize = "score_mean", budget = 100):
+    ## Primeiro alguns filtros
+    actual_row = df.illoc[[row]]
+    
+    if(actual_row["status"] != "Provável"):
+        return math.inf
+    # if(actual_row["stat;:us"] != ""):
